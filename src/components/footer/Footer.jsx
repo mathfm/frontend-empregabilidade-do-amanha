1f1562256caf664bc48612c422b5b60e4301e119
Samuel Martins <125680404+Samue1-Martins@users.noreply.github.com>
2024-05-06 15:57:53 -0300

    feat: adding responsiveness

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -4,8 +4,8 @@ import { FaInstagram, FaLinkedin } from "react-icons/fa6";
 export const Footer = () => {
   return (
     <footer className={styles["footer"]}>
-      <figure className="img-footer">
-        <img src="/src/assets/logo-site.svg" alt="" />
+      <figure className={styles["img-footer"]}>
+        <img src="/src/assets/logo-site.svg" alt="" className="img-footer" />
       </figure>
       <div className={styles["group-info"]}>
         <div className={styles["group-icon"]}>
